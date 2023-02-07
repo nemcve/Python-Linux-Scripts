df3e6b8f13ff8b41c274772dd01ee61d47fb0f34
Nemanja Cvejić <37965912+nemcve@users.noreply.github.com>
2023-02-07 18:17:13 +0100

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-# script that lists all files in a directory, including all files in subfolders of that directory
-
+# Script that lists all files in a directory, including all files in subfolders of that directory
+# Useful when used with grep
+# Usage python3 script1.py | grep ".terraform"
 
 import os
-
-
 for dirname, pathname, filename in os.walk("/home/Nemanja"):
     print(dirname)
     for file in filename:
