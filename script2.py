806978eb07f3a20d4fdb9f751f7cb12f2cb3b3de
Nemanja Cvejić <37965912+nemanja023@users.noreply.github.com>
2023-02-10 15:55:10 +0100

    script3.py, comments

--- script2.py
+++ script2.py
@@ -1,19 +1,20 @@
 #!usr/bin/python3
 
-# Python script for searching for files in Linux based on the user input                                                  
-# Usage: python3 script2.py /etc passwd                                                                                                                                                                                                               
-
-import os                                                                                                                  
-import argparse                                                                                                                                                                                                                                       
-
-
-parser = argparse.ArgumentParser(description="Reading your input..")                                                       
-parser.add_argument("pathsearch", help="Enter path name to search into: ")                                                 
-parser.add_argument("filesearch", help="Enter filename to search for: ")                                                   
-args = parser.parse_args()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+# Python script for searching for files in Linux based on the user input
+# Usage: 
+# Linux - python3 script2.py /etc passwd
+# Windows python3 script2.py C:/Users/Nemanja/Desktop script2.py
 
 
-for dirname, pathname, filename in os.walk(args.pathsearch):                                                                   
-    for file in filename:                                                                                                          
-        if file == args.filesearch:                                                                                                  
-            print(os.path.join(dirname,file)) 
+import os
+import argparse
+parser = argparse.ArgumentParser(description="Reading your input")
+parser.add_argument("pathsearch", help="Enter path name to search into: ")
+parser.add_argument("filesearch", help="Enter filename to search for: ")
+args = parser.parse_args()
+
+
+for dirname, pathname, filename in os.walk(args.pathsearch):
+    for file in filename:
+        if file == args.filesearch:
+            print(os.path.join(dirname, file))
---
+++ script3.py
@@ -0,0 +1,18 @@
+# Search inside the directories and their subdirectories for the files with a specific extension
+# Usage:
+# Linux - python script3.py /home/Nemanja .txt | less
+# Windows - python script3.py C:/Users/Nemanja .txt | out-host -paging
+
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description="Waiting for your input...")
+parser.add_argument("pathsearch", help="Please enter a directory to search: ")
+parser.add_argument("extensionfinder",
+                    help="Please enter a file extension to search: ")
+args = parser.parse_args()
+
+for dirname, dirpath, filename in os.walk(args.pathsearch):
+    for file in filename:
+        if file.endswith(args.extensionfinder):
+            print(os.path.join(dirname, file))
